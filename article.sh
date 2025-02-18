645fc64750e9b6a82af859a60853ec5c9a67a915
Tyler Maginnis <tylermaginnis@users.noreply.github.com>
2025-02-18 14:02:43 -0500

    Add article: 1._Best_chimney_sweep_services_in_Louisville_KY.html to unprocessed_articles directory

--- article.sh
+++ article.sh
@@ -41,6 +41,36 @@ for file in unprocessed_articles/*.html; do
     fi
 done
 
+#!/bin/bash
+
+# Read the template from articles.html
+template=$(cat article_template.html)
+
+# Read the TOC template from articles_toc_template.html
+toc_template=$(cat articles_toc_template.html)
+
+# Initialize an empty string to store the TOC content
+toc_content=""
+
+# Loop through all HTML files in the articles directory
+for file in articles/*.html; do
+    # Check if the file is a regular file (not a directory)
+    if [ -f "$file" ]; then
+        # Extract the filename without the path
+        filename=$(basename "$file")
+
+        # Extract the date from the filename
+        date=$(basename "$file" | cut -d'_' -f1)
+        
+        # Create a link for the TOC
+        name=$(basename "$file" | cut -d'_' -f2- | sed 's/\.html$//' | tr '_' ' ')
+        link="<a href='articles/${filename}'>${name}</a><br>"
+        
+        # Append the link to the TOC content
+        toc_content="${toc_content}${link}"
+    fi
+done
+
 # Replace the {TOC} token in the TOC template with the TOC content
 new_toc_content="${toc_template//ARTICLES_TOC/${toc_content}}"
 
