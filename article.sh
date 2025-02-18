05491c397a3e66679ffef96bdfea0e6a4a982ff3
Tyler Maginnis <tylermaginnis@users.noreply.github.com>
2025-02-18 13:24:49 -0500

    Add article: 1._Best_chimney_sweep_services_in_Louisville_KY.html to articles directory

---
+++ article.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Read the template from articles.html
+template=$(cat article_template.html)
+
+# Read the TOC template from articles_toc_template.html
+toc_template=$(cat articles_toc_template.html)
+
+# Initialize an empty string to store the TOC content
+toc_content=""
+
+# Loop through all HTML files in the articles directory
+for file in articles/*.html; do
+    # Check if the file is a regular file (not a directory)
+    if [ -f "$file" ]; then
+        # Read the content of the file
+        content=$(cat "$file")
+        
+        # Replace the {ARTICLE} token in the template with the content
+        new_content="${template//ARTICLE_TEMPLATE/${content}}"
+        
+        # Overwrite the file with the new content
+        echo "$new_content" > "$file"
+        
+        # Extract the date from the filename
+        date=$(basename "$file" | cut -d'_' -f1)
+        
+        # Create a link for the TOC
+        link="<a href='${file}'>${date}</a><br>"
+        
+        # Append the link to the TOC content
+        toc_content="${toc_content}${link}"
+    fi
+done
+
+# Replace the {TOC} token in the TOC template with the TOC content
+new_toc_content="${toc_template//ARTICLES_TOC/${toc_content}}"
+
+# Overwrite the articles_toc.html file with the new TOC content
+echo "$new_toc_content" > articles_toc.html
