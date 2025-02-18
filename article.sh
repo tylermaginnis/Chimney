6c42dc5e862548f9ec7e68b54b8e570b37cc4647
Tyler Maginnis <tylermaginnis@users.noreply.github.com>
2025-02-18 14:29:46 -0500

    Fix generation

--- article.sh
+++ article.sh
@@ -7,7 +7,7 @@ template=$(cat article_template.html)
 toc_template=$(cat articles_toc_template.html)
 
 # Initialize an empty string to store the TOC content
-toc_content=""
+toc_content="<ul>"
 
 # Loop through all HTML files in the unprocessed_articles directory
 for file in unprocessed_articles/*.html; do
@@ -32,25 +32,11 @@ for file in unprocessed_articles/*.html; do
         # Extract the date from the filename
         date=$(basename "$file" | cut -d'_' -f1)
         
-        # Create a link for the TOC
-        name=$(basename "$file" | cut -d'_' -f2- | sed 's/\.html$//' | tr '_' ' ')
-        link="<a href='articles/${filename}'>${name}</a><br>"
-        
-        # Append the link to the TOC content
-        toc_content="${toc_content}${link}"
+
     fi
 done
 
-#!/bin/bash
-
-# Read the template from articles.html
-template=$(cat article_template.html)
-
-# Read the TOC template from articles_toc_template.html
-toc_template=$(cat articles_toc_template.html)
-
-# Initialize an empty string to store the TOC content
-toc_content=""
+# Remove the first TOC generation loop
 
 # Loop through all HTML files in the articles directory
 for file in articles/*.html; do
@@ -61,13 +47,16 @@ for file in articles/*.html; do
         
         # Create a link for the TOC
         name=$(basename "$file" | sed 's/_/ /g' | sed 's/\.html$//')
-        link="<ahref ='articles/${filename}'>${name}</a><br>"
+        link="<li><a href='articles/${filename}'>${name}</a></li>"
 
         
         # Append the link to the TOC content
         toc_content="${toc_content}${link}"
     fi
 done
+
+# Close the unordered list
+toc_content="${toc_content}</ul>"
 
 # Replace the {TOC} token in the TOC template with the TOC content
 new_toc_content="${toc_template//ARTICLES_TOC/${toc_content}}"
